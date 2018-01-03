f59303754ae48e0e070b6e105fed436f3bd9760c
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-01-03 08:42:26 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -380,7 +380,7 @@ bot.on('message', function (user, userID, channelID, message, evt)
 
 bot.on('presence', function(user, userID, status, game, event)
 {
-   bot.sendMessage({to: DData.clipChan, message: user});	
+   //bot.sendMessage({to: DData.clipChan, message: user});	
 });
 
 function getUserTranLang(userID)
