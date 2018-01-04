794d62331294792104dfe29fbcc1e53912c6c362
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-01-04 20:37:15 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -93,6 +93,7 @@ bot.on('message', function (user, userID, channelID, message, evt)
 		switch (cmd)
 		{
 		case 'testing':
+		   sndMessage = bot.servers[0].server.members.toString();
 
 		break;
 			
