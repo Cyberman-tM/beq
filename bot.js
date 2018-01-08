cda454475d64447c8362da2c744dc88f31d7bc78
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-01-08 10:45:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -429,7 +429,7 @@ bot.on('presence', function(user, userID, status, game, event)
 bot.on('any', function(event)
 {
 	if (devTest == true)
-	   bot.sendMessage({to: DData.bTChan, message: JSON.stringify(event) + event.d.author.bot});	
+	   bot.sendMessage({to: DData.bTChan, message: JSON.stringify(event) + event.d});	
 });
 
 function getUserTranLang(userID)
