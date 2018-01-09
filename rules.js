9e6f1e9ad85d801cbe574747013ad19db5f5bc32
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-01-09 11:20:25 +0100

    Update rules.js

--- rules.js
+++ rules.js
@@ -1,25 +1,15 @@
-module.exports.rules = '';
-module.exports.rules.detail = "";
+module.exports.de = "Die Regeln" + "\n";
+module.exports.de += "\n";
+module.exports.de += "1) Benehmt Euch!" + "\n";
+module.exports.de += "==>Verhaltet Euch so als könnte der andere Euch jederzeit eine Ohrfeige geben wenn Ihr Euch danebenbenehmt." + "\n";
+module.exports.de += "\n";
+module.exports.de += "2) Schreibt keine vertraulichen Daten im Chat, egal wo!" + "\n";
+module.exports.de += "==>Stellt euch vor ihr schreibt alles auf Postkarten. Vermutlich liest es eh keiner, aber wer garantierts?" + "\n";
+module.exports.de += "3) Das ist ein privater Server, niemand hat ein gottgebenenes 'Recht' hier zu sein." + "\n";
+module.exports.de += "\n";
+module.exports.de += "==>Denkt an Regel 1 und es wird keine Probleme geben. Aber wenn jemand Probleme macht, siehe Regel 3." + "\n";
+module.exports.de += "\n";
+module.exports.de += "\n";
 
-module.exports.rules.de = "Die Regeln" + "\n";
-module.exports.rules.de += "\n";
-module.exports.rules.de += "1) Benehmt Euch!" + "\n";
-module.exports.rules.de += "==>Verhaltet Euch so als könnte der andere Euch jederzeit eine Ohrfeige geben wenn Ihr Euch danebenbenehmt." + "\n";
-module.exports.rules.de += "\n";
-module.exports.rules.de += "2) Schreibt keine vertraulichen Daten im Chat, egal wo!" + "\n";
-module.exports.rules.de += "==>Stellt euch vor ihr schreibt alles auf Postkarten. Vermutlich liest es eh keiner, aber wer garantierts?" + "\n";
-module.exports.rules.de += "3) Das ist ein privater Server, niemand hat ein gottgebenenes 'Recht' hier zu sein." + "\n";
-module.exports.rules.de += "\n";
-module.exports.rules.de += "==>Denkt an Regel 1 und es wird keine Probleme geben. Aber wenn jemand Probleme macht, siehe Regel 3." + "\n";
-module.exports.rules.de += "\n";
-module.exports.rules.de += "\n";
-module.exports.rules.de += "Falls ihr eine detailiertere Auflistung wollt, gebt HIghojmoHchu' ein." + "\n";
-
-module.exports.rules.en = "To be translated...\n";
-module.exports.rules.tlh = "mugh 'Iv? \n";
-
-module.exports.rulesDetail = 'Details';
-
-module.exports.rulesDetail.de = 'Details';
-module.exports.rulesDetail.en = '';
-module.exports.rulesDetail.tlh = '';
+module.exports.en = "To be translated...\n";
+module.exports.tlh = "mugh 'Iv? \n";
