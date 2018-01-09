3919ac89b8013cfb7ba21cf149646b12a926001e
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-01-09 10:18:37 +0100

    Create rules.js

---
+++ rules.js
@@ -0,0 +1,16 @@
+module.exports.rules.de = "Die Regeln" + "\n";
+module.exports.rules.de += "\n";
+module.exports.rules.de += "1) Benehmt Euch!" + "\n";
+module.exports.rules.de += "==>Verhaltet Euch so als könnte der andere Euch jederzeit eine Ohrfeige geben wenn Ihr Euch danebenbenehmt." + "\n";
+module.exports.rules.de += "\n";
+module.exports.rules.de += "2) Schreibt keine vertraulichen Daten im Chat, egal wo!" + "\n";
+module.exports.rules.de += "==>Stellt euch vor ihr schreibt alles auf Postkarten. Vermutlich liest es eh keiner, aber wer garantierts?" + "\n";
+module.exports.rules.de += "3) Das ist ein privater Server, niemand hat ein gottgebenenes 'Recht' hier zu sein." + "\n";
+module.exports.rules.de += "\n";
+module.exports.rules.de += "==>Denkt an Regel 1 und es wird keine Probleme geben. Aber wenn jemand Probleme macht, siehe Regel 3." + "\n";
+module.exports.rules.de += "\n";
+module.exports.rules.de += "\n";
+module.exports.rules.de += "Falls ihr eine detailiertere Auflistung wollt, gebt HIghojmoHchu' ein." + "\n";
+
+module.exports.rules.en = "To be translated...\n";
+module.exports.rules.tlh = "mugh 'Iv? \n";
