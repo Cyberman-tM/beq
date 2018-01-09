3d848cc0071f3be33efa5dc69dcc54e120c20913
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-01-09 11:25:39 +0100

    Update rules.js

--- rules.js
+++ rules.js
@@ -4,11 +4,11 @@ module.exports.de += "1) Benehmt Euch!" + "\n";
 module.exports.de += "==>Verhaltet Euch so als könnte der andere Euch jederzeit eine Ohrfeige geben wenn Ihr Euch danebenbenehmt." + "\n";
 module.exports.de += "\n";
 module.exports.de += "2) Schreibt keine vertraulichen Daten im Chat, egal wo!" + "\n";
+module.exports.de += "\n";
 module.exports.de += "==>Stellt euch vor ihr schreibt alles auf Postkarten. Vermutlich liest es eh keiner, aber wer garantierts?" + "\n";
-module.exports.de += "3) Das ist ein privater Server, niemand hat ein gottgebenenes 'Recht' hier zu sein." + "\n";
 module.exports.de += "\n";
+module.exports.de += "3) Das ist ein privater Server, es gibt kein 'Recht' hier zu sein." + "\n";
 module.exports.de += "==>Denkt an Regel 1 und es wird keine Probleme geben. Aber wenn jemand Probleme macht, siehe Regel 3." + "\n";
-module.exports.de += "\n";
 module.exports.de += "\n";
 
 module.exports.en = "To be translated...\n";
