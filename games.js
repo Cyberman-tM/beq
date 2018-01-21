f908c6d7be20b3f95044edc2a7fbef77b24169a3
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-01-21 15:57:17 +0100

    Add files via upload

--- games.js
+++ games.js
@@ -10,6 +10,7 @@ module.exports.gameTalkDef = JSON.stringify(
 {
 	"userID": "",         //Current user
 	"playGame": "",       //Game he wants to play
+	"cmd": "",            //command or parameters to the game
 	"message": "",        //Return message of the game (current score, you win, etc...)
 	"noGame": false,      //No game was requested, error
 	"reserved": "nothing" //There may be more to come
@@ -19,13 +20,14 @@ module.exports.gameTalkDef = JSON.stringify(
 var userGameList = new Array();
 var aIdx = null;
 
-module.exports.runGames = function(bot, userID, message, sndMessage)
+module.exports.runGames = function(bot, userID, message)
 {
 	var args = message.substring(1).split(' ');
 	var cmd = args[0];
 	var gameTalk = JSON.parse(module.exports.gameTalkDef);
 	gameTalk.userID = userID;
 	
+	gameTalk.message = "You should never see this! Something went wrong...";
 		//This also sets aIdx!
 	var userGame = getUGL(userID);
 	if (aIdx == null)
@@ -39,32 +41,27 @@ module.exports.runGames = function(bot, userID, message, sndMessage)
 		}
 		else
 		{
-			var gameRun = wamaH.init();
+			var gameRun = wamaH.init(module.exports.gameTalkDef);
 			//Noch kein aktives Spiel, User will Spiel XY aktivieren:
 			userGameList.push(
 			{
 				userID: userID,
 				game: "wa'maH",
-				cmd: gameRun,
-				score: '0'				
+				cmd: gameRun
 			});
-			sndMessage = wamaH.initGame;
+			sndMessage = wamaH.preGame;
 		}		
 	}
 	else
 	{
 		//We know the user is currently playing a game				
 		userGame = userGame[0];
-		sndMessage = userGame.cmd();
-	}
-	
+		gameTalk.cmd = message.substring(1);
+		gameTalk = userGame.cmd();
+	}	
 
-	
-	
-	gameTalk.message = sndMessage;
 	return gameTalk;
 }
-
 
 function getUGL(userID)
 {
--- wamaH.js
+++ wamaH.js
@@ -8,39 +8,70 @@
 
 var userGames = new Array();
 var aIdx = null;
+var gameTalkDef = "";
 
-module.exports.initGame = 'maj. DaH maQujchoH!\n';
-module.exports.initGame += 'Rules: write % and either wa\' or cha\', followed by a blank and the TOTAL score so far.\n';
-module.exports.initGame += "Whoever reaches wa'maH first has won. If you want beq to begin, start with *%pagh pagh*";
+var userGameDef = JSON.stringify(
+{
+	"userID": "",
+	"curCount": 0
+});
+
+module.exports.preGame = 'maj. DaH maQujchoH!\n';
+module.exports.preGame += 'Rules: write % and either wa\' or cha\', followed by a blank and the TOTAL score so far.\n';
+module.exports.preGame += "Whoever reaches wa'maH first has won. If you want beq to begin, start with *%pagh pagh*";
 
 //Does this overwrite other run exports?
-module.exports.run = function(bot, userID, message, sndMessage)
+module.exports.run = function(gameTalk)
 {
-	var args = message.substring(1).split(' ');
-	var cmd = args[0];
+	//All we should get is two numbers
+	var args = gameTalk.cmd.split(' ');
+	var num1 = args[0];
+	var num2 = args[1];
 	
-	sndMessage = 'nothing';
-	//This also sets aIdx!
-	var userGame = getUserGame(userID);
-	if (aIdx == null)
+	//We need TWO parameters
+	if (num1 == undefined || num2 == undefined)
 	{
+		gameTalk.noGame = true;
+		gameTalk.message = "Missing parameters, try again!";
 	}
-	else
+	else if (!isNaN(Num1) || !isNaN(Num2))
 	{
-		var userVal = translateWord2Number(args[1]);
-		var userScore = translateWord2Number(args[2]);
+		gameTalk.noGame = true;
+		gameTalk.message = "Qo'! tlhIngan Hol yIghItlhnIS!";
+	}
+	
+	
+	//Only if we didn't already determine the input is faulty
+	if (gameTalk.noGame == false)
+	{
+		//This also sets aIdx!
+		var userGame = getUserGame(userID);	
+		if (aIdx == null)
+		{
+			//Prepare new game board
+			var UG = JSON.parse(userGameDef);
+			UG.userID = gameTalk.userID;
+			UG.curCount = 0;
+			userGames.push(UG);
+			
+			userGame = getUserGame(userID);			
+		}
+		
+		userGame = userGame[0];
+		var userVal = translateWord2Number(num1);
+		var userScore = translateWord2Number(num2);
 		if (userVal > 2 ||
-		    ( userGame.score + userVal !=  userScore ) ||
+			( userGame.curCount + userVal !=  userScore ) ||
 			userScore > 10 )
 		{
-			sndMessage = "Qo'! bIluj.";
+			gameTalk.message = "Qo'! bIluj.";
 			
 			//Remove user
 			userGames = userGames.splice(aIdx, 1);
 		}
 		else if ( userScore == 10 )
 		{
-			sndMessage = "majQa'!";
+			gameTalk.message = "majQa'!";
 		}
 		else
 		{
@@ -48,30 +79,31 @@ module.exports.run = function(bot, userID, message, sndMessage)
 			var RNG = Math.random() * 100;
 			var aiScore = 0;
 			
-			if (userGames.score < 8)				
+			if (userGames.curCount < 8)				
 				if (RNG > 50)
 					aiScore = 2;
 				else
 					aiScore = 1;
 			else
-				aiScore = 10 - userGames.score;
+				aiScore = 10 - userGames.curCount;
 			
-			userGames.score += aiScore;
+			userGames.curCount += aiScore;
 					
-			if (userGames.score == 10)
-				sndMessage = "Qapla'! jIDunqu'! :-)";
+			if (userGames.curCount == 10)
+					gameTalk.message = "Qapla'! jIDunqu'! :-)";
 			else
-				sndMessage = translateWord2Number(aiScore) + ' ' + translateWord2Number(userGames.score);
+				gameTalk.message = translateWord2Number(aiScore) + ' ' + translateWord2Number(userGames.curCount);
 		}
 	}
-	
-	return sndMessage;
+
+return gameTalk;
 }
 
 
 //Return the actual "run" routine that does the game work
-module.exports.init = function()
+module.exports.init = function(p_gameTalkDef)
 {
+	gameTalkDef = p_gameTalkDef;
 	return module.exports.run;
 }
 
@@ -82,7 +114,7 @@ function getUserGame(userID)
 		if (UG.userID == userID)
 		{
 			aIdx = iIdx;
-			return UT
+			return UG
 		}
 	}
 	);
