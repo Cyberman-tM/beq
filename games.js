062109c63d592db2c118b5f9345e1043c1a120d9
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-01-21 09:51:51 +0100

    Add files via upload

--- games.js
+++ games.js
@@ -23,7 +23,7 @@ module.exports.runGames = function(bot, userID, message, sndMessage)
 {
 	var args = message.substring(1).split(' ');
 	var cmd = args[0];
-	var gameTalk = JSON.parse(games.gameTalkDef);
+	var gameTalk = JSON.parse(gameTalkDef);
 	
 		//This also sets aIdx!
 	var userGame = getUGL(userID);
