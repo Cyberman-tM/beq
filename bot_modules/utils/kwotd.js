6ee0facefdb78a483447934baa322ef2c4340d7f
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-05-01 20:11:21 +0200

    work on timer

--- bot_modules/utils/kwotd.js
+++ bot_modules/utils/kwotd.js
@@ -6,10 +6,33 @@
    
 */
 
+var KWOTDTimings = require('./kwotd_timings.js');
+
 module.exports.KWOTD = function()
 {
+	var thisDate = Date();
+	
 	//We are called by the universal timer - which runs once a minute
 	//So we need to check if we are supposed to start yet
+	
+	//First lets see if there are any timings to run at all
+	var myTimings = JSON.parse(KWOTDTimings.KWOTDTimings);
+	if (myTimings.length == 0)
+	   return;
+	var myHour = thisDate.getHours();
+	var myMinu = thisDate.getMinutes();
+	var waHour = '';
+	var waMinu = '';
+	
+	myTimings.forEach(item)
+	{
+	   waHour = item.time.substr(0,2);
+	   waMinu = item.time.substr(3,2);
+	   if (waHour == myHour && waMinu == myMinu)
+	   {
+	      //Run KWOTD, send to channel
+	   }
+	}
 	
 	
 }
