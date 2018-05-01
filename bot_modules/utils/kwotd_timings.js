2c599737b74d7f2f0cb1d4b5ae53bd7c2c575402
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-05-01 20:07:25 +0200

    Update kwotd_timings.js

--- bot_modules/utils/kwotd_timings.js
+++ bot_modules/utils/kwotd_timings.js
@@ -3,6 +3,7 @@
     
     Easier to maintain if in a separate file :-)
     
+    Meaning of time: at this time the KWOTD should run - note JavaScript notation: 0-23, 0-59
     Meaning of type: type of word like in boQwI', except when preceded by a !, then it's negated
     
     The KWOTD event will check the time, and if it's less than a minute after it, it'll run    
