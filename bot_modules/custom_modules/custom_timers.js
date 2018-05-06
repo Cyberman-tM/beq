a2995bc7ab9872a09aad7b137a8bde3c631fc000
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-05-06 17:28:10 +0200

    Update custom_timers.js

--- bot_modules/custom_modules/custom_timers.js
+++ bot_modules/custom_modules/custom_timers.js
@@ -2,8 +2,15 @@
    Custom timers
 */
 
+//Initialize the custom timer, reserver space, etc..
+module.exports.runCustInit = function()
+{
+	
+}
+
 //The name is mandatory!
-module.exports.runCusTimer = function()
+//myDate is a Date object, myHour and myMinute are Date.getHours and Date.getMinutes
+module.exports.runCusTimer = function(myDate, myHour, myMinute)
 {
 	//You get no parameters, you are just called
 	//Now include whatever coding you want to run - maybe call another module?
