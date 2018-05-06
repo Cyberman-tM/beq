d6770ed136ace3f824148ac6d1d32045b9de1868
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-05-06 18:43:50 +0200

    Update kwotd_timings.js

--- bot_modules/utils/KWOTD/kwotd_timings.js
+++ bot_modules/utils/KWOTD/kwotd_timings.js
@@ -13,7 +13,7 @@ module.exports.KWOTDTimings = JSON.stringify(
 [
    { "time": "08:00",
      "type": "n" },
-   { "time": "18:56",
+   { "time": "16:46",
      "type": "n" },
   { "time": "16:00",
      "type": "v" },
