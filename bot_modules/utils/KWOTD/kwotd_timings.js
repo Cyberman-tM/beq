985d0d4309a606ff2870fcc943c11d90f19366e9
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-05-09 19:25:58 +0200

    Update kwotd_timings.js

--- bot_modules/utils/KWOTD/kwotd_timings.js
+++ bot_modules/utils/KWOTD/kwotd_timings.js
@@ -9,11 +9,13 @@
     Meaning of time: at this time the KWOTD should run - note JavaScript notation: 0-23, 0-59
     Meaning of type: type of word like in boQwI', separate multiple choices with a |
     
-    The KWOTD event will check the time, and if it's less than a minute after it, it'll run    
+    The KWOTD event will check the time, and if it's less than a minute after it, it'll run
+    A note for Heroku: currently Heroku is 2 hours behind my time (Vienna time).
+    So there's probably a shift at least once a year, this is not accounted for. Yet :-)
 */
 
 module.exports.KWOTDTimings = JSON.stringify(
 [
-   { "time": "8:1", "type": "n|v" },
-   { "time": "10:1", "type": "v:pref|v:suff|n:suff" }   
+   { "time": "6:1", "type": "n|v" },
+   { "time": "17:1", "type": "v:pref|v:suff|n:suff" }   
 ]);
