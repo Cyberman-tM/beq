edbb3514ef5a581e933ca44990babb64b288f3fe
Rejutka Lupex <cyberman_the_mastermind@hotmail.com>
2018-05-12 13:06:34 +0200

    restart_npm_modules

--- bot_modules/utils/weather.js
+++ bot_modules/utils/weather.js
@@ -4,24 +4,17 @@
 */
 
 var TDData  = require('./../external/discord_data.js');
-var request = require('request-promise');
 var logger = require('winston');
 
 //We have partial asynchronous behaviour, we need a global variable to check progress
 var weatherResponse = null;
 module.exports.getWeather = function(cityIDs)
 {	
-
 	//Initialize for this run
 	weatherResponse = null;
 	var weatherErr = null;
 	var weatherURL = 'http://api.openweathermap.org/data/2.5/weather?id=cityID&appid=_APPID_';
 	
-	logger.info("wetter");
-
-	weatherURL = weatherURL.replace('cityID', cityIDs);
-	weatherURL = weatherURL.replace('_APPID_', TDData.openWeatherMap);
-	logger.info(weatherURL);
 	/*
 	const response = await fetch('https://api.com/values/1');
 	const resp = await request(weatherURL, function (err, resp, body)
@@ -36,18 +29,6 @@ module.exports.getWeather = function(cityIDs)
 	readWeather();
 	*/
 	
-	logger.info("x");
-	const request = async () => {
-    const response = await fetch('https://api.com/values/1');
-    const json = await response.json();
-    logger.info(json);
+
 }
 
-request();
-logger.info("y");
-	
-	logger.info('end');
-	logger.info(weatherErr);
-	logger.info(weatherResponse);
-	
-}
