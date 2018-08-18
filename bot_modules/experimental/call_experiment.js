48bb7801fc1453e1192d92dccd929b1e32b9127c
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-08-18 20:51:50 +0200

    Update call_experiment.js

--- bot_modules/experimental/call_experiment.js
+++ bot_modules/experimental/call_experiment.js
@@ -1,5 +1,5 @@
 //Wrapper for experimentation - a branch in bot.js will recognize the command "experiment" (prefixed with !, or $!, or whatever),
-//and call this function:
+//and call this function
 
 //The parameters are placeholders - JavaScript doesn't check for type, so you can give anything
 module.exports = function(parameter1, parameter2, parameter3)
