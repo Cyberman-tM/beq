8be08fc0aac52c8a2aa2f84f349deb901158440f
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-08-18 17:52:03 +0200

    Update sendMessage.js

--- bot_modules/utils/sendMessage.js
+++ bot_modules/utils/sendMessage.js
@@ -1,7 +1,10 @@
 //send method, wrapper for discord.js (or whatever follows? :-)
 
-module.exports.botSendMailText = function(bot, channelID, messageString)
+module.exports.botSendMailText = function(messageType, bot, channelID, messageString)
 {
    //This should work in discord.js
-   bot.channels[channelID].send(messagestring);   
+   if (messageType == 1)
+      bot.channels[channelID].send(messagestring);   
+   else
+      //Die silently...
 }
