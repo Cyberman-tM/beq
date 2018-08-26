5b902bf9c3f5fcd69a13140df6de0d61889d20b3
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-08-26 17:10:11 +0200

    Update gameTemplate.js

--- bot_modules/games/gameTemplate.js
+++ bot_modules/games/gameTemplate.js
@@ -5,3 +5,15 @@
 var gameTalkDef = require('./gameTalkDef.js');
 
 module.exports.preGame = 'Message to the user befor the game starts, rules and such...';
+module.exports.gameInfo = 'Message about game version, for startup of game engine';
+
+module.exports.init = function(gameTalkDef)
+{
+   return run;
+};
+
+function run(gameTalkDef)
+{
+};
+
+module.exports.
