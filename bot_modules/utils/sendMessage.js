d0fdd2ebeb530c4ec7972ec0e1ba4887dc67d6f8
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-08-26 16:06:13 +0200

    Update sendMessage.js

--- bot_modules/utils/sendMessage.js
+++ bot_modules/utils/sendMessage.js
@@ -2,10 +2,6 @@
    var logger = require('winston');
 module.exports = function(messageType, bot, channelID, messageString)
 {
-
-   logger.info(channelID);
-   logger.info(messageString);
-   logger.info(bot.channels);
    //This should work in discord.js
    if (messageType == 1)
       bot.channels.get(channelID).send(messageString);   
