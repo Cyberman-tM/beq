6e877b9484ce4071c37d91b6619ed77529d18106
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-11-23 00:58:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,6 +13,7 @@ var evTimer = require('./bot_modules/utils/event_timer.js');
 var weather = require('./bot_modules/utils/weather.js');
 var KWOTD   = require('./bot_modules/utils/KWOTD/kwotd.js');
 var botSendMessage = require ('./bot_modules/utils/sendMessage.js');
+var experimentalFunc = require ('/bot_modules/experimental/currentExperiment.js');
 
 //Internal version - package.json would contain another version, but package.json should never reach the client,
 //so it's easier to just have another version number in here...
@@ -359,6 +360,9 @@ bot.on('message', function (messageDJS)
 		   sndMessage = NumWords.Word2Num(onePar);
 		break;
 			//Übersetzungen
+		case 'experiment':
+			experimentalFunc(args);
+		break;
 		case 'mugh':
 			var talkBeq = JSON.parse(beq.beqTalkDef);
 		
