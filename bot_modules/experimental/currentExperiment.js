e1280e757969eb8005a3f2323339def073e674ca
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-11-23 02:01:47 +0100

    Update currentExperiment.js

--- bot_modules/experimental/currentExperiment.js
+++ bot_modules/experimental/currentExperiment.js
@@ -7,7 +7,7 @@ module.exports = (args, messageDJS) =>
     .then(function(messages)
           {
           logger.info(`Received ${messages.size} messages`);
-          logger.info(messages[0].content);
+          logger.info(messages.array()[0].content);
           })    
     .catch(console.error);
 }
