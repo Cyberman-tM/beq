0bee086ef6f4eedb4a604b676cf578011596c142
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-11-23 01:42:57 +0100

    Update currentExperiment.js

--- bot_modules/experimental/currentExperiment.js
+++ bot_modules/experimental/currentExperiment.js
@@ -3,9 +3,5 @@ var logger = require('winston');
 module.exports = (args, messageDJS) => 
 {
   logger.info(args[0]);
-  logger.info(messageDJS.channel.ID);
-  logger.info(messageDJS.channel);
-  messageDJS.channel.fetchPinnedMessages()
-  .then(messages => console.log(`Received ${messages.size} messages`))
-  .catch(console.error);
+  messageDJS.channel.fetchPinnedMessages().then(messages => console.log(`Received ${messages.size} messages`)).catch(console.error);
 }
