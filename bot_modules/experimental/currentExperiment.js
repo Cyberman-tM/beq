df220bbf6d5377a6c529a6fbf8f821dba91122d7
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-11-24 00:06:49 +0100

    Update currentExperiment.js

--- bot_modules/experimental/currentExperiment.js
+++ bot_modules/experimental/currentExperiment.js
@@ -9,12 +9,26 @@ module.exports = (bot, args, messageDJS) =>
   LMChannel.fetchPinnedMessages()
   .then(function(messages)
   {
+    if (messages.size > 0)
+    {
     //Derzeit kann es nur eine geben!
     var newMessage = messages.array()[0].content + messageDJS.content;    
     logger.info(newMessage);
     messages.array()[0].edit(newMessage)
   .then(msg => console.log(`New message content: ${msg}`))
   .catch(console.error);
+    }
+    else
+    {
+      //New message
+      // Send a basic message
+channel.send(messageDJS.content)
+  .then(function(message)
+        {
+      message.pin();
+      })
+  .catch(console.error);
+    }
   })
   /*
   messageDJS.channel.fetchPinnedMessages()
