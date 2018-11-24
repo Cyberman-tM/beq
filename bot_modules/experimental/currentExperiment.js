1f6aefc4eec6f844aec1fe8f2a77114f7f386a46
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-11-24 11:48:45 +0100

    Update currentExperiment.js

--- bot_modules/experimental/currentExperiment.js
+++ bot_modules/experimental/currentExperiment.js
@@ -5,42 +5,31 @@ module.exports = (bot, args, messageDJS) =>
 {
   var LMChannel = bot.channels.get(DData.LMChan);
   
+  //curMessage hat noch den Befehl - wie werden wir den los?
+  var curMessage = messageDJS.content;
+  
   //Holen wir uns die gepinnten Nachrichten aus dem Letter to Maltz Channel
   LMChannel.fetchPinnedMessages()
   .then(function(messages)
   {
+	//Wir haben schon Nachrichten, wir nehmen momentan nur die erste!
     if (messages.size > 0)
     {
-    //Derzeit kann es nur eine geben!
-    var newMessage = messages.array()[0].content + messageDJS.content;    
-    logger.info(newMessage);
-    messages.array()[0].edit(newMessage)
-  .then(msg => console.log(`New message content: ${msg}`))
-  .catch(console.error);
+		//Bestehende Nachricht mit neuer Nachricht verknüpfen:
+		var newMessage = messages.array()[0].content + '\n' + curMessage;
+		
+		//Bestehende Nachricht editieren
+		messages.array()[0].edit(newMessage)
+	    .then()
+	    .catch(console.error);
     }
-    else
+    else		
     {
-      //New message
-      // Send a basic message
-LMChannel.send(messageDJS.content)
-  .then(function(message)
-        {
-      message.pin();
-      })
-  .catch(console.error);
+      //New pinned message
+	  LMChannel.send(messageDJS.content)
+	  .then(function(message)
+      {message.pin();})
+	  .catch(console.error);
     }
   })
-  /*
-  messageDJS.channel.fetchPinnedMessages()
-    .then(function(messages)
-          {
-          logger.info(`Received ${messages.size} messages`);
-          logger.info(messages.array()[0].content);
-          })    
-    .catch(console.error);
-  */
-  
-  
-  
-  
 }
