9735df489993c51e01972b57ed94cf5571b88e92
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-12-19 09:55:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -115,14 +115,13 @@ bot.on('message', function (messageDJS)
 	// That is: command (translate) language (klingon) word (Suv)
 	if ( message.substring(0, 1) == '!' || message.substring(0, 1) == '?')
 	{		
-		logger.info(message.substring(2,5));
 		//Special processing, there are shortcut commands, we have to translate them to normal commands
 		if (message.substring(0, 1) == '?')
 		{
 			if (channelID == DData.clipChan)
 			{
 				//Inside the "ask beq" Channel, we always want to show notes when asking for a klingon word:
-				if (message.substring(2,5) == 'tlh')
+				if (message.substring(1,4) == 'tlh')
 				   beqTalk.showNotes = true;
 				
 				//A ? always means "mugh", translate. And must be followed by the language, without space.
