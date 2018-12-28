35644bd40bc21f02a52512758549a263eb84f10d
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-12-28 11:11:39 +0100

    Update search_canon.js

--- bot_modules/commands/search_canon.js
+++ bot_modules/commands/search_canon.js
@@ -1,10 +1,15 @@
 var logger = require('winston');
 var DData = require('./../external/discord_data.js');
+var klingonskaCanonLink = "http://klingonska.org/canon/search/?q=";
 
 module.exports = (bot, args, messageDJS) => 
 {
    var tmpRet = "";
-   //Do stuff
+   
+   //Assume anything beyond the parameter itself is supposed to be the argument for the Klingonska Canon search
+   args[0] = "";
+   var searchWords = args.join();  //Separator?
+   tmpRet = klingonskaCanonLink + searchWords;   
    
    return tmpRet;
 }
