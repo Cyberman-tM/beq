078ae0094dc75a41ff29bbc2349741bb6b8fa896
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-12-28 11:21:16 +0100

    Update search_canon.js

--- bot_modules/commands/search_canon.js
+++ bot_modules/commands/search_canon.js
@@ -8,7 +8,7 @@ module.exports = (bot, args, messageDJS) =>
    
    //Assume anything beyond the parameter itself is supposed to be the argument for the Klingonska Canon search
    args[0] = "";
-   var searchWords = args.join();  //Separator?
+   var searchWords = args.join('');  //Separator?
    tmpRet = klingonskaCanonLink + searchWords;   
    
    return tmpRet;
