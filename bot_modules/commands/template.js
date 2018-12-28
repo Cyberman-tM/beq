9f3c10e9f9329a2cecd74748ea4e8b59208a4d6a
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-12-28 11:06:11 +0100

    Create template.js

---
+++ bot_modules/commands/template.js
@@ -0,0 +1,10 @@
+var logger = require('winston');
+var DData = require('./../external/discord_data.js');
+
+module.exports = (bot, args, messageDJS) => 
+{
+   var tmpRet = "";
+   //Do stuff
+   
+   return tmpRet;
+}
