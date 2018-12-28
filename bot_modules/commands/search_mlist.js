13b4155054088eecdef8009cb29cbc9f0fb37c44
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-12-28 14:36:41 +0100

    Create search_mlist.js

---
+++ bot_modules/commands/search_mlist.js
@@ -0,0 +1,24 @@
+ar logger = require('winston');
+var DData = require('./../external/discord_data.js');
+var oldKLIMailList = "https://www.kli.org/sphider/search.php?query=[query]&search=1";
+var newKLIMailList = "lists.kli.org/mmsearch.cgi/tlhingan-hol-kli.org?config=tlhingan-hol-kli.org&restrict=&exclude=&method=and&format=short&sort=score&words=[query]";
+
+module.exports = (bot, args, messageDJS) => 
+{
+   var tmpRet = "";
+   
+   //Assume anything beyond the parameter itself is supposed to be the argument for the search
+   args[0] = "";
+   var searchWords = args.join('');  //Separator?
+   
+   tmpRet = "KLI Mailing List, up to 2015:\n";
+   tmpRet += oldKLIMailList.replace("[query]", searchWords);
+   tmpRet += "\n\n";
+   tmpRet += "KLI Mailing List, beyond 2015:\n";
+   tmpRet += newKLIMailList.replace("[query]", searchWords);
+   tmpRet += "\n\n";
+   
+   //Add in personality lines
+   
+   return tmpRet;
+}
