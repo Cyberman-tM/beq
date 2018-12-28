e08185ee331d3bd8c4511a84edbce469a723762d
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-12-28 17:06:04 +0100

    Create searchWiki.js

---
+++ bot_modules/commands/searchWiki.js
@@ -0,0 +1,16 @@
+var logger = require('winston');
+var DData = require('./../external/discord_data.js');
+var wikiLink = "http://www.klingonwiki.net/";
+
+module.exports = (bot, args, messageDJS) => 
+{
+   var tmpRet = "";
+   
+   //first parameter MUST be the language - we'll simply assume it is, and that it's an existing language
+   var lang = args[1].toLowerCase();
+   lang.substring(0,1) = lang.substring(0,1).toUpperCase();
+   
+   tmpRet = wikiLink + lang + '/' args[2];
+   
+   return tmpRet;
+}
