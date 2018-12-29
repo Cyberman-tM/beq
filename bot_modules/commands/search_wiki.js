452553014b49c14b394bf1d94e3cffd36d1d0bc0
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-12-29 21:47:36 +0100

    Update search_wiki.js

--- bot_modules/commands/search_wiki.js
+++ bot_modules/commands/search_wiki.js
@@ -6,8 +6,10 @@ module.exports = (bot, args, messageDJS) =>
 {
    var tmpRet = "";
    
-   //first parameter MUST be the language - we'll simply assume it is, and that it's an existing language
    var lang = args[1].toLowerCase();
+   if (lang != 'de' && lang != 'en' && lang != 'fr' && lang != 'nl' && lang != 'es')
+      lang = 'de';
+   
    lang.substring(0,1) = lang.substring(0,1).toUpperCase();
    
    tmpRet = wikiLink + lang + '/' + args[2];
