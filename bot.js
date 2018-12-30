949866dd0467a3aa903cd77de29518cf68443afa
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-12-30 20:03:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -243,7 +243,7 @@ bot.on('message', function (messageDJS)
 			}
 			break;
 		case "yIcha'":
-			sndMessage = showAffixes(bot, args, messageDJS, beq.engine.KDBVPJson, beq.engine.KDBVSJson, beq.engine.KDBNSJson);			
+			sndMessage = showAffixes(bot, args, messageDJS, beq.KDBVPJson, beq.KDBVSJson, beq.KDBNSJson);			
 		break;			
 		case 'linkMe':
 		   var ListLink1 = args[1];
