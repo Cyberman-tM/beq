eb8206ba6a25580be6b7ae4d170f03c40673b42a
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2018-12-30 19:54:08 +0100

    Create show_affixes.js

---
+++ bot_modules/commands/show_affixes.js
@@ -0,0 +1,10 @@
+var logger = require('winston');
+var DData = require('./../external/discord_data.js');
+
+module.exports = (bot, args, messageDJS, KDBVPJSon, KDBVSJson, KDBNSJson) => 
+{
+   var tmpRet = "Nothing implemented yet";
+   //Do stuff
+   
+   return tmpRet;
+}
