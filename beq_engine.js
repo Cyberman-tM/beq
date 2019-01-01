08d1f368654e16ee0fe075d3bca8a649da8b8778
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2019-01-01 02:22:05 +0100

    Update beq_engine.js

--- beq_engine.js
+++ beq_engine.js
@@ -366,6 +366,7 @@ module.exports.Engine = function(beqTalk)
 			if (beqTalk.wordType1 == "prefix")
 			{
 				var ycount = 0;
+				logger.info(beqTalk.result.length);
 				//Simply export all prefixes
 				//Do a loop because we might want to limit it some day (i.e. all prefixes using "he", for example)
 				module.exports.KDBVPJSon.forEach(function(onePref)
