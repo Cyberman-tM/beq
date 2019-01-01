4dc415a3ac649373b619d35d7afbd39f61bf4d7f
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2019-01-01 01:40:47 +0100

    Update beq_engine.js

--- beq_engine.js
+++ beq_engine.js
@@ -54,6 +54,7 @@ You must initialize it before calling beq! Some fields may have default entries!
 
 */
 var kTranscode = require('./bot_modules/utils/recode.js');
+
 var fs = require('fs');
 var xmldoc = require('xmldoc');
 
