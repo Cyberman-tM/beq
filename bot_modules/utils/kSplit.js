fe6166adec748a111f7694c4cc5ff6da2523a3ce
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2019-04-22 18:56:37 +0200

    Create kSplit.js

---
+++ bot_modules/utils/kSplit.js
@@ -0,0 +1,15 @@
+/*
+  Split a text into syllables/words
+  For example:
+  tlhIngan vIjatlh vIneH
+  ->tlhINgan vI- jatlh neH
+  
+*/
+
+module.exports.versInt = '0.1';
+module.exports.nameInt = 'Klingon text split';
+
+module.exports.RCtlh2x = function(raw_text, split_syls)
+{
+
+}
