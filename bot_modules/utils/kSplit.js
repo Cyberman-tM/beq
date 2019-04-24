4642efc48a98402b1fdaa41d265e2ab0827afa88
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2019-04-24 13:36:27 +0200

    Update kSplit.js

--- bot_modules/utils/kSplit.js
+++ bot_modules/utils/kSplit.js
@@ -14,5 +14,13 @@ module.exports.nameInt = 'Klingon text split';
 module.exports.kSplit = function(raw_text, split_syls)
 {
   var rawRecode = kTranscodeX.RCu22tlh(raw_text);
+  
+  //Alle nicht-Worte durch Leerzeichen ersetzen (Punkt, Beistrich, etc...)
+  var onlyWordsRE = new RegExp(/[^a-zA-Z0-9]/, 'g');
+  
+  var wordsOnly = sourcestring.replace(onlyWordsRE, ' ');
+  var wordList = wordsOnly.split(' ');
+  
+
 
 }
