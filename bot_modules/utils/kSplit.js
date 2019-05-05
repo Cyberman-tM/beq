2da6871258c4603a76706c9b34f9a664e3df84c9
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2019-05-05 07:53:30 +0200

    Update kSplit.js

--- bot_modules/utils/kSplit.js
+++ bot_modules/utils/kSplit.js
@@ -30,13 +30,62 @@ module.exports.kSplit = function(raw_text, split_syls)
 //Sort through the wordlist:
 var undefList = [];
 var verbList = [];
+var prefList = [];
+var checkSuff = false;
 	
 wordList.forEach(function(oneWord)
 {
+  //We have to check for the entire word in any case, unfortunately
+  undefList.push(oneWord);
+	
+  //Always assume suffixes
+  checkSuff = true;
+
   if (oneWord.length <= 3)
   {
       //Whatever it is, it cannot have affixes, so we don't know if it's noun or verb or whatever
-      undefList.push(oneWord);	  
+      checkSuff = false;
+  }
+ //Longer than 3 characters, might have a prefix, which means 4th place must be a number
+ else if (oneWord.substring(3,4) > 0)
+  {
+	  //Quite likely a prefix, therefore verb (or a nounified verb)
+	  //We already will check the whole word, so lets see if we find a verb prefix:
+	  switch (oneWord.substring(0,1))
+	  {
+		//Prefixes in Uhmal 2
+		case 'b3':
+		case 'b4':
+		case 'c2':
+		case 'c4':
+		case 'd1':
+		case 'd3':
+		case 'd5':
+		case 'f4':
+		case 'g3':
+		case 'i3':
+		case 'i5':
+		case 'j3':
+		case 'j5':
+		case 'k1':
+		case 'k5':
+		case 'l3':
+		case 'l5':
+		case 'o2':
+		case 'o3':
+		case 'q1':
+		case 'r2':
+		case 's1':
+		case 's5':
+		case 't3':
+		case 't5':
+		case 'w3':
+		case 'w3':
+		case 'y3':
+		   prefList.push(oneWord.substring(0,1));
+		   oneWord = oneWord.substring(1);
+		  break;
+	  }
   }
          //Check if the first letter CANNOT be a prefix:
   else if (oneWord.substring(0,1) == "u" || 
@@ -44,18 +93,9 @@ wordList.forEach(function(oneWord)
   {
       //Might be a noun, might be a verb - we don't know
   }
-  else
-  {
-      //Longer than 3 characters, might have a prefix
-      if (oneWord.substring(3,4) > 0)
-      {
-	  //Quite likely a prefix, therefore verb
-	  verbList.push(oneWord);
-      }
-  }
  });
 	
- return verbList.join();
+ return prefList.join();
 
 
 }
