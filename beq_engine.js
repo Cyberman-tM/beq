3467ec28ce9b4c613ef5f7e38a825727062b210c
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2019-12-14 20:42:05 +0100

    Update beq_engine.js

--- beq_engine.js
+++ beq_engine.js
@@ -1166,10 +1166,11 @@ requestify.get('http://www.tlhingan.at/Misc/beq/wordCat/beq_Categories.txt').the
     module.exports.catDataWords = new Array();
     module.exports.catDataCategs = new Array();    
         
-        logger.info(xmlDoc.children);
     xmlDoc.children.forEach(function (word)
     {
-        logger.info(word);
+        logger.info(word.attr.name);
+        logger.info(word.val);
+        logger.info("...");
         
         var wordName = word.getAttribute("name");
         var wordCats = word.innerHTML;
