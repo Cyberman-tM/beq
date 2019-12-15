b8acf64be2598e6ec98c529baa48dca71403ae30
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2019-12-15 21:38:40 +0100

    Update categorize.js

--- bot_modules/commands/categorize.js
+++ bot_modules/commands/categorize.js
@@ -8,7 +8,7 @@ module.exports = function(beq_engine, dataString)
 {
     //First word will be the word we want to categorize, second word (after blank)
     //will be the categorie we want to add
-    var args[] = dataString.split(' ');
+    var args = dataString.split(' ');
     
     logger.info(beq_engine.KDBJSon);
     logger.info(beq_engine.exports.KDBJSon);
