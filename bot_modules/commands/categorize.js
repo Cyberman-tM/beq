d9a15d4aa215ba5f445d4eb0151e48ce9134f884
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2019-12-15 21:51:10 +0100

    Update categorize.js

--- bot_modules/commands/categorize.js
+++ bot_modules/commands/categorize.js
@@ -6,12 +6,45 @@ var logger = require('winston');
 
 module.exports = function(beq_engine, dataString)
 {
+    var tmpRet = "Nothing done.";
+    
     //First word will be the word we want to categorize, second word (after blank)
     //will be the categorie we want to add
+    //third, if exists, is the number of the result we want
     var args = dataString.split(' ');
+        				
+	//TODO: search with boundary? only single word?
+	var RE = new RegExp(regexLook, regexFlag);
+	var results = beq_engine.KDBJSon.filter(function (item)
+	{
+        //Always look in english!
+		return item["en"].match(RE);
+	});
     
-    logger.info(beq_engine.KDBJSon);
-    logger.info(beq_engine.exports.KDBJSon);
+    //results hat jetzt möglicherweise mehrere Einträge
+    if (results.length > 1)
+    {
+        //TODO: newline - where from? beqtalk?
+        tmpRet = "Multiple matches!" + "\n";
+        
+        if (args[2] != null)
+        {
+            var realResult = null;
+            realResult = results[args[2]];
+            if (realResult == undefined)
+                tmpRet += "Requested match #" + args[2] + "not found.";
+            else
+            {
+                var chkWord = realResult.tlh + ";;" + realResult.type;
+                if (beq_engine.catDataWords[chkWord] != undefined)
+                {
+                    tmpRet += "Found category for word:" + beq_engine.catDataWords[chkWord];
+                }
+                else
+                    tmpRet += "Category not found - will be added!";
+            }
+        }
+    }
     
-    return dataString;
+    return tmpRet;
 }
