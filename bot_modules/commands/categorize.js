ef5c17237e15569b9a52afdc879d07760455f4d7
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2019-12-15 21:01:23 +0100

    Update categorize.js

--- bot_modules/commands/categorize.js
+++ bot_modules/commands/categorize.js
@@ -1,1 +1,10 @@
 
+/*
+  Categorize - sort words into different categories, addendum to boQwI' database
+*/
+var logger = require('winston');
+
+module.exports = function(beq_engine, dataString)
+{
+    logger.info(beq_engine);
+}
