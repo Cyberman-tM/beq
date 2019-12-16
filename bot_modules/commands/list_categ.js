f4bf0e650cd951652267a1a1c432437f4318d29b
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2019-12-17 00:20:02 +0100

    Update list_categ.js

--- bot_modules/commands/list_categ.js
+++ bot_modules/commands/list_categ.js
@@ -1,1 +1,26 @@
 
+/*
+  List available categories, without words
+  Sorted alphabetically
+*/
+
+module.exports = function(beq_engine)
+{
+    var tmpRet = "";
+    
+    var allCats = Object.keys(beq_engine.catDataCategs).sort();
+    var lastChar = '';
+    
+    tmpRet = "Currently available categories:\n```";
+    allCats.forEach(function(item)
+    {
+        if (item.substr(0,1) != lastChar)
+        {
+            lastChar = item.substr(0,1);
+            tmpRet += "**Categories starting with" + lastChar + "**\n";
+        }
+        tmpRet += item + "\n";
+    });
+    tmpRet += "```";
+    return tmpRet;    
+}
