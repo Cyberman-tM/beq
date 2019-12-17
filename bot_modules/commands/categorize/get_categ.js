07b1cd56cbad8de394057321ecba3ce026671a8a
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2019-12-17 09:47:43 +0100

    beq_engine, nicht beg_engine
    Ich brauch mehr Schlaf...

--- bot_modules/commands/categorize/get_categ.js
+++ bot_modules/commands/categorize/get_categ.js
@@ -4,41 +4,30 @@ var xmldoc = require('xmldoc');
 
 module.exports = function(beq_engine)
 {
-	logger.info("func");
 	//Reorg call
 	requestify.get('http://www.tlhingan.at/Misc/beq/wordCat/beq_reorgCat.php').then(function (response)
 	{
-		logger.info("org");
 		//Response from reorg is irrelevant, but there's no use reading the XML before it has been created...
 		requestify.get('http://www.tlhingan.at/Misc/beq/wordCat/beq_Categories.txt').then(function (response)
 		{
-			logger.info("dat");
 			// Get the response body
 			var document = new xmldoc.XmlDocument(response.getBody());
 			logger.info("afterxml");
 
 			//Reset, just to be sure
-			beg_engine.catDataWords = {};
+			beq_engine.catDataWords = {};
 			beg_engine.catDataCategs = {};
 			
-			logger.info("x");
-			logger.info(document);
-			logger.info(beq_engine.catDataWords);
-			logger.info("y");
-			
+
 			var words = document.childrenNamed("w");
 			words.forEach(function (word)
 			{
-				logger.info(word);
 				var wordName = word.attr.name;
 				var wordCats = word.val;
 
 				//Worte sollten einzigartig sein
 				beq_engine.catDataWords[wordName] = wordCats;
 				
-				logger.info(beq_engine.catDataWords);
-				logger.info("x");
-
 				//Kategorien sind definitiv nicht einzigartig
 				var categs = wordCats.split(";");
 				categs.forEach(function (oneCateg)
