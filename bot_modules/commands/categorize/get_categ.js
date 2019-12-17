08fa8fd553deaa865adbb18e71fc8a473a5f35a1
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2019-12-17 09:17:42 +0100

    Update get_categ.js

--- bot_modules/commands/categorize/get_categ.js
+++ bot_modules/commands/categorize/get_categ.js
@@ -12,6 +12,7 @@ module.exports = function(beq_engine)
 		//Response from reorg is irrelevant, but there's no use reading the XML before it has been created...
 		requestify.get('http://www.tlhingan.at/Misc/beq/wordCat/beq_Categories.txt').then(function (response)
 		{
+			logger.info("we have data");
 			// Get the response body
 			var document = new xmldoc.XmlDocument(response.getBody());
 
