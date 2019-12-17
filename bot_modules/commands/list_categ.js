cda079221d4c4c5ee9d35fb4c62d6875e73a595f
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2019-12-17 06:11:04 +0100

    Update list_categ.js

--- bot_modules/commands/list_categ.js
+++ bot_modules/commands/list_categ.js
@@ -14,13 +14,16 @@ module.exports = function(beq_engine)
     tmpRet = "Currently available categories:\n```";
     allCats.forEach(function(item)
     {
-      tmpRet += "_" + item + "_";
+      //Woher kommt der leere Eintrag?
+      if (item != "")
+      {
         if (item.substr(0,1) != lastChar)
         {
             lastChar = item.substr(0,1);
             tmpRet += "**Categories starting with " + lastChar + "**\n";
         }
         tmpRet += item + " (" + beq_engine.catDataCategs[item].length  + ")\n";
+      }
     });
     tmpRet += "```";
     return tmpRet;    
