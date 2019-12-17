ff9530518972b21d023c3b49c41eb563548a2476
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2019-12-17 09:57:54 +0100

    Update show_categ.js

--- bot_modules/commands/categorize/show_categ.js
+++ bot_modules/commands/categorize/show_categ.js
@@ -13,9 +13,9 @@ module.exports = function(beq_engine, lookWord)
     tmpRet = "Words in category " + lookWord + "\n";
     beq_engine.catDataCategs[lookWord].forEach(function(item)
     {
-    //TODO: prepare output
+    //TODO: prepare output, add translation?
       var tmpCat = item.split(";;");
-       tmpRet += tmpCat[0] + boQwI_translate.getWType(tmpCat[1].substr(0,1), "en") + "\n";
+       tmpRet += tmpCat[0] + " " + boQwI_translate.getWType(tmpCat[1].substr(0,1), "en") + "\n";
     });
     
     return tmpRet;    
