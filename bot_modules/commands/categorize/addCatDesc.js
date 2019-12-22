2e333691757b14c69b49896078f27627f0aa3dc1
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2019-12-22 21:18:57 +0100

    Update addCatDesc.js

--- bot_modules/commands/categorize/addCatDesc.js
+++ bot_modules/commands/categorize/addCatDesc.js
@@ -4,16 +4,21 @@ Add a description to the category
 
  */
 var requestify = require('requestify'); 
+var logger = require('winston');
 
 module.exports = function (beq_engine, beqMessage)
 {
 	var tmpRet = "";
 	var args = beqMessage.split(" ");
 	var category = args[1];
+	
+	logger.info(category);
+	logger.info(beqMessage);
 
 	//Clear command and category
 	args[0] = "";
 	args[1] = "";
+	
 
 	var catDesc = args.join(" ");
 
