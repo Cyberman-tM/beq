ee6f3a474131b71b42625f4a9df1eb8fb021a5f6
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2019-12-22 21:23:27 +0100

    Update addCatDesc.js

--- bot_modules/commands/categorize/addCatDesc.js
+++ bot_modules/commands/categorize/addCatDesc.js
@@ -11,20 +11,17 @@ module.exports = function (beq_engine, beqMessage)
 	var tmpRet = "";
 	var args = beqMessage.split(" ");
 	var category = args[1];
-	
-	logger.info(category);
-	logger.info(beqMessage);
 
 	//Clear command and category
 	args[0] = "";
 	args[1] = "";
 	
-
 	var catDesc = args.join(" ");
 
-	//TODO: write to file, re-read xml
+	//TODO: re-read xml
 	var addCatLink = "http://www.tlhingan.at/Misc/beq/wordCat/beq_catadddesc.php?category=" + category + "&description=" + catDesc;
 	requestify.get(addCatLink);
 
+	tmpRet = "Done.";
 	return tmpRet;
 }
