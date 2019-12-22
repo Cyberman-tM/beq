0706879c10c8fdbecd246f991c156f2b90875c90
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2019-12-22 21:08:20 +0100

    Update addCatDesc.js

--- bot_modules/commands/categorize/addCatDesc.js
+++ bot_modules/commands/categorize/addCatDesc.js
@@ -1,24 +1,24 @@
 /*
 
-  Add a description to the category
+Add a description to the category
 
-*/
+ */
 
-module.exports = function(beq_engine, beqMessage)
+module.exports = function (beq_engine, beqMessage)
 {
-    var tmpRet = "";
-    var args = beqMessage.split(" ");
-    var category = args[1];
-    
-    //Clear command and category
-    args[0] = "";
-    args[1] = "";
-    
-    var catDesc = args.join(" ");
-    
-    //TODO: write to file, re-read xml
-    
-    
-    
-    return tmpRet;    
+	var tmpRet = "";
+	var args = beqMessage.split(" ");
+	var category = args[1];
+
+	//Clear command and category
+	args[0] = "";
+	args[1] = "";
+
+	var catDesc = args.join(" ");
+
+	//TODO: write to file, re-read xml
+	var addCatLink = "http://www.tlhingan.at/Misc/beq/wordCat/beq_catadddesc.php?category=" + category + "&description=" + catDesc;
+	requestify.get(addCatLink);
+
+	return tmpRet;
 }
