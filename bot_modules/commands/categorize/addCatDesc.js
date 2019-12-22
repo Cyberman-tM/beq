6c82b30c4e55ff0ded2a5b1aef6d2790bca10bd2
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2019-12-22 18:30:16 +0100

    Update addCatDesc.js

--- bot_modules/commands/categorize/addCatDesc.js
+++ bot_modules/commands/categorize/addCatDesc.js
@@ -3,3 +3,22 @@
   Add a description to the category
 
 */
+
+module.exports = function(beq_engine, beqMessage)
+{
+    var tmpRet = "";
+    var args = beqMessage.split(" ");
+    var category = args[1];
+    
+    //Clear command and category
+    args[0] = "";
+    args[1] = "";
+    
+    var catDesc = args.join(" ");
+    
+    //TODO: write to file, re-read xml
+    
+    
+    
+    return tmpRet;    
+}
