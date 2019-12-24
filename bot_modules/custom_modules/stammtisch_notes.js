8725b6696f0c4533e8e38d683202c93a50ec19fc
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2019-12-24 09:23:48 +0100

    Create stammtisch_notes.js

---
+++ bot_modules/custom_modules/stammtisch_notes.js
@@ -0,0 +1,15 @@
+{
+	"items": [{
+			"weekday": 6,
+			"hour": 19,
+			"minute": 0,
+			"message": "test 6 19 0"
+		},
+		{
+			"weekday": 6,
+			"hour": 18,
+			"minute": 0,
+			"message": "test 6 18 0"
+		}
+	]
+}
