b2dc4af6b0ed03f07e376b50849f6d77f0d68280
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2019-12-26 01:05:03 +0100

    Update stammtisch_notes.js

--- bot_modules/custom_modules/stammtisch_notes.js
+++ bot_modules/custom_modules/stammtisch_notes.js
@@ -3,13 +3,15 @@
 			"weekday": 5,
 			"hour": 19,
 			"minute": 0,
-			"message": "test 5 19 0"
+			"message": "test 5 19 0",
+		        "channel": "650385585642405907"
 		},
 		{
 			"weekday": 5,
 			"hour": 18,
 			"minute": 0,
 			"message": "test 5 18 0"
+			"channel": "650385585642405907"
 		}
 	]
 }
