2996045937aa95abc3b5e85dd4efba73105b5b15
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2020-01-01 20:23:42 +0100

    Update show_categ.js

--- bot_modules/commands/categorize/show_categ.js
+++ bot_modules/commands/categorize/show_categ.js
@@ -40,7 +40,7 @@ function getSingleCategory (beq_engine, lookWord)
 	else
 	{
 		tmpRet = "Category " + lookWord + "\n";
-		tmpRet += "*" + beq_engine.catDesc[lookWord].trim() + "*\n";
+		tmpRet += "*" + beq_engine.catDesc[lookWord].trim() + "*\n\n";
 		
 		beq_engine.catDataCategs[lookWord].forEach(function (item)
 		{
