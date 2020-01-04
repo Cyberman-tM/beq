d7502189dccfa364b10079baa2918a0f07959fbe
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2020-01-04 03:07:22 +0100

    Update reReadKDB.js

--- bot_modules/commands/categorize/reReadKDB.js
+++ bot_modules/commands/categorize/reReadKDB.js
@@ -48,6 +48,7 @@ module.exports = function(beq_engine)
                 beq_engine.catDataWords[chkWord] == undefined || 
                !beq_engine.catDataWords[chkWord].includes(newCategory))
 	    {
+		    logger.info("newWord");
 		//Store as uhmal
 	        chkWord = kTranscode.RCtlh2u(item.tlh) + ";;" + item.type;		    
                 requestify.get("http://www.tlhingan.at/Misc/beq/wordCat/beq_addCategory.php?wordKey=" + chkWord +  "&wordCat=" + newCategory);
