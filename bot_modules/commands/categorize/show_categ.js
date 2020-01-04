25461b56237b8c94eadca6ef1e17c8008743f70a
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2020-01-04 20:55:08 +0100

    Update show_categ.js

--- bot_modules/commands/categorize/show_categ.js
+++ bot_modules/commands/categorize/show_categ.js
@@ -29,7 +29,7 @@ module.exports = function (beq_engine, lookWord)
         else
         {
             if (firstEl.includes("_"))
-                if (firstEl.startsWith(secEl));
+                if (firstEl.startsWith(secEl))
                    tmpRet = -1;
             else if (secEl.includes("_"))
                 if (secEl.startsWith(firstEl))
