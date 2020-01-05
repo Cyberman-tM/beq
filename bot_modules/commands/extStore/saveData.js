5399672733fd259db6d0e930d7b15f8b364fc43a
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2020-01-05 20:23:51 +0100

    Update saveData.js

--- bot_modules/commands/extStore/saveData.js
+++ bot_modules/commands/extStore/saveData.js
@@ -1,38 +1,40 @@
 /*
-   Save data on external server
-   The data is not processed, just stored in a file with the keyname
-*/
+Save data on external server
+The data is not processed, just stored in a file with the keyname
+ */
 
 var winston = require('winston');
-var logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  transports: [
-    new winston.transports.Console()
-  ]
-});
-var requestify = require('requestify'); 
+var logger = winston.createLogger(
+	{
+		level: 'info',
+		format: winston.format.json(),
+		transports: [
+			new winston.transports.Console()
+		]
+	}
+	);
+var requestify = require('requestify');
 
-module.exports = function(dataKey, dataData)
+module.exports = function (dataKey, dataData)
 {
-    var dataString = JSON.stringify(dataData);
-    logger.info("instore");
-    //Daten irgendwie ändern? Prüfen? Blubb
-   /*
-    requestify.post("http://www.tlhingan.at/Misc/beq/storage/store.php", dataString)
-      .then(function(response)
-                                                                                                             {
-    logger.info("response");
-       logger.info(response.getBody());
-    })
-    .fail(function(response)
-    {logger.info("fail");
-    logger.info(response.getCode())});
-    */
-   
-requestify.post("http://www.tlhingan.at/Misc/beq/storage/store.php", {name: "Hiago", age: 23}, {
-    dataType: 'form-url-encoded'
-}).then(function(res) {
-    console.log(res);
-});
+	var dataString = JSON.stringify(dataData);
+	//Daten irgendwie ändern? Prüfen? Blubb
+
+	requestify.post("http://www.tlhingan.at/Misc/beq/storage/store.php?dataKey=" + dataKey, dataString,
+	{
+		dataType: 'form-url-encoded'
+	})
+	.then(function (response)
+	{
+		logger.info("response");
+		logger.info(response.getBody());
+	})
+	.fail(function (response)
+	{
+		logger.info("fail");
+		logger.info(response.getCode())
+	});
+
+}
+);
 }
