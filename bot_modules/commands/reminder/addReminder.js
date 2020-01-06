391d262ef05a2e4af63fcca479b8e7b2fc7f9127
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2020-01-06 12:43:20 +0100

    Create addReminder.js

---
+++ bot_modules/commands/reminder/addReminder.js
@@ -0,0 +1,18 @@
+/*
+
+   Add another reminder
+   Read in stored reminder, add this one
+   
+   Parameters:
+   Time to display
+   Which channel
+   Message
+   Repeat how often?
+   Repeat when?
+   
+*/
+
+module.exports = function()
+{
+    
+}
