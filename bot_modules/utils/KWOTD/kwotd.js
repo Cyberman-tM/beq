20be08d1d7b0220e5923df0acef7c8d7339907c2
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2020-02-15 22:32:11 +0100

    Update kwotd.js

--- bot_modules/utils/KWOTD/kwotd.js
+++ bot_modules/utils/KWOTD/kwotd.js
@@ -94,7 +94,7 @@ module.exports.KWOTDTranslate = function(beqTalk, item)
 		 else
 		     wordType = boQwITranslate.getWType(item.type, 'en');		      
 
-		 sndMessage += 'tlhIngan: ** *' + item.tlh + '* ***' + beqTalk.newline + beqTalk.newline;
+		 sndMessage += 'tlhIngan: ** *' + item.tlh + '* **' + beqTalk.newline + beqTalk.newline;
 	
 		 sndMessage += 'Type of word: *' + wordType + '*' + beqTalk.newline;
 		 sndMessage += 'English: *' + item.en + '*' + beqTalk.newline;
