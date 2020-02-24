8624c8cd8b1347873ea6e500b44cca960af3193b
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2020-02-24 21:31:06 +0100

    Update kSplit.js

--- bot_modules/utils/kSplit.js
+++ bot_modules/utils/kSplit.js
@@ -24,15 +24,18 @@ module.exports.nameInt = 'Klingon text split';
 
 module.exports.kSplit = function (raw_text)
 {
+	logger.info(raw_text);
     var tmpText = "";
 	//Alle nicht-Worte durch Leerzeichen ersetzen (Punkt, Beistrich, etc...)
 	var wordsOnly = raw_text.replace(/[^a-zA-Z0-9]/g, ' ');
+	logger.info(wordsOnly);
 
 	//We have uhmal 3 for this stuff now
 	//uhmal  replaces double-letters with single letters
 	//uhmal2 replaces vowels with numbers
 	//uhmal3 replaces consonant clusters with AEI
 	var wordsUhmal3 = kTranscodeX.RCtlh2u3(wordsOnly);
+	logger.info(wordsUhmal3);
 
 	//Split into obvious separate words and remove duplicates
 	var wordList = wordsUhmal3.split(' ');
@@ -41,6 +44,7 @@ module.exports.kSplit = function (raw_text)
     var prefix = "";
 	wordList.forEach(function (oneWord)
     {
+		logger.info(oneWord);
        	if (oneWord.length >= 5)
         {
             if (parseInt(oneWord.substring(3,4)) > 0 &&
@@ -51,12 +55,15 @@ module.exports.kSplit = function (raw_text)
             }		
             prefix = prefix.replace('g', '6');
         }
+	logger.info(prefix);
+		logger.info(oneWord);
         var syls = oneWord.split(/([a-z][1-5][a-z])/);
         if (syls.length > 0)
         {
             oneWord = "";
             syls.forEach(function(syllable)
             {
+		    logger.info(syllable);
                 oneWord = syllable + "-";
             });
         }
