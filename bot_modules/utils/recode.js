513d379420ab5ee1f75642139e85bfcab28c69f9
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2020-02-24 20:53:13 +0100

    Update recode.js

--- bot_modules/utils/recode.js
+++ bot_modules/utils/recode.js
@@ -259,7 +259,7 @@ module.exports.RC2Morska = function(orig_text)
 	var prefix = '';
 	if (tmpText.length >= 5)
 	{
-
+logger.info(tmpText.substring(0,2));
 		if (Number.isInteger(tmpText.substring(3,4)) == true &&
 		    prefixList.indexOf(tmpText.substring(0,2)) != -1 )
 		{
