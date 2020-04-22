967596a5a598dd9faca9525d4e58eb518ef82aac
Rejutka Lupex <cyberman@fredlarts.at>
2020-04-22 23:01:37 +0200

    .

--- bot_modules/commands/categorize20/reloadcats.js
+++ bot_modules/commands/categorize20/reloadcats.js
@@ -35,7 +35,7 @@ module.exports = function(beq_engine)
         // Get the response body
         logger.info("in response");        
         var  catData = JSON.parse(response.getBody());
-        logger.info(catData.KID);
+        logger.info(catData[0].KID);
 
     });
     tmpRet = "done?";
