00daadfaf635b026e4034d9bb3fdfcac0a406416
Rejutka Lupex <cyberman@fredlarts.at>
2020-04-22 20:13:22 +0200

    .

--- bot_modules/commands/categorize20/reloadcats.js
+++ bot_modules/commands/categorize20/reloadcats.js
@@ -1,8 +1,33 @@
 /*
    Reload categorization
 */
+var requestify = require('requestify'); 
+var catAPI = require('./../../external/cat_api.js');
 
 module.exports = function(beq_engine)
 {
-    return "Yup, called it!";
+    var tmpRet = "";
+
+    requestify.get(catAPI.catGetData + "?dataID=categs").then(function (response)
+    {
+        /*
+        KID
+        [
+           KID (again)
+           fn - fullname
+           WIDS
+           [
+              WID
+              JDescs[lang] text
+           ]
+        ]
+        */
+        // Get the response body
+        var catList = JSON.parse(response.getBody());
+
+        tmpRet = catList[0].KID + catList[0].fn;
+
+    });
+
+    return tmpRet;
 }
