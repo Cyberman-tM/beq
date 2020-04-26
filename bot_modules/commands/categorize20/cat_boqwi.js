a483eb746e2ec3699985c2fe1ef2b81beda5c408
Rejutka Lupex <cyberman@fredlarts.at>
2020-04-26 16:04:56 +0200

    .

--- bot_modules/commands/categorize20/cat_boqwi.js
+++ bot_modules/commands/categorize20/cat_boqwi.js
@@ -137,6 +137,7 @@ module.exports = function (beq_engine) {
 
                 var fromBoq = encodeURI("Taken from boQwI\'");
                 //Create category (maybe it exists already, doesn't matter, we get the ID anyway)
+                /*
                 var createCatURL = catAPI.catCreateCat + "&catName=" + itemCat + "&catDLan=en" + "&catDesc=" + fromBoq;
                 requestify.get(createCatURL).then(function (response) {
                     //Response should be the category id
@@ -151,6 +152,7 @@ module.exports = function (beq_engine) {
                     });
 
                 });
+                */
             });
         }
 
