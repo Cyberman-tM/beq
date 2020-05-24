b2505b6ed1243431ba8aae21e6910640d202bd33
Rejutka Lupex <cyberman@fredlarts.at>
2020-05-24 09:08:49 +0200

    .

--- bot.js
+++ bot.js
@@ -164,10 +164,9 @@ function processMessage(bot, messageDJS) {
 	//React to special emoji
 	else if (cmdMagic == '<' && message.substring(1, 3) == "beq")
 	{
-		logger.info(message);
-		if (message.substring(0, 12) == ":beq_en2tlh:>") {
+		if (message.substring(0, 12) == ":beq_en2tlh:") {
 			cmdMagic = '!';
-			message = message.substring(30);
+			message = message.substring(31);
 			message = "mugh en" + message;
 		}
 	}
