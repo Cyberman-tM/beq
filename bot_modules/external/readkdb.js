175f104eb627c25966c70dcaad5be13848f6ce31
Rejutka Lupex <Cyberman-tM@users.noreply.github.com>
2022-01-08 13:34:49 +0100

    Update readkdb.js

--- bot_modules/external/readkdb.js
+++ bot_modules/external/readkdb.js
@@ -72,8 +72,6 @@ module.exports = function (KDBJSon, KDBPHJSon, KDBVPJSon, KDBVSJSon, KDBNSJSon, 
                         break;
                     case 'part_of_speech':
                         emptyStruct.type = item.firstChild.text;
-                      logger.info(emptyStruct.type);
-                      logger.info(emptyStruct.type.startsWith("n:"));
                         if (emptyStruct.type == "n" || emptyStruct.type.startsWith("n:") )
                            KDBNCount++;
                         else if (emptyStruct.type == "v" || emptyStruct.type.startsWith("v:") )
